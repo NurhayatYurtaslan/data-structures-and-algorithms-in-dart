83a4d9cc59a954d1d3cd4d984c09d82f5c3eefde
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-07-27 15:20:10 +0300

    Update list.dart

---  basic_data_structures_in_dart/list.dart
+++  basic_data_structures_in_dart/list.dart
@@ -1,8 +1,15 @@
 void main() {
 final people = ['Pablo', 'Manda', 'Megan'];
   print(people);
-  //add people
+  //Insertion Location
   people.add("Edith");
   print(people);
+  //add multiple elements
+  people..add("Diana")..add("Suzana")..add("Mark");
+  print(people);
+  //add to index
+  people.insert(0,"Hanso");
+  print(people);
+  
   
 }
