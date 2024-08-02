315ca802a4fc2038904dc722afc046f636c23ddc
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-08-02 14:35:38 +0300

    Update starter.dart

--- linked_list/lib/starter.dart
+++ linked_list/lib/starter.dart
@@ -1,10 +1,12 @@
+import 'linked_list.dart';
 
+void main() {
+  final node1 = Node<int>(1, value: 1);
+  final node2 = Node<int>(2, value: 2);
+  final node3 = Node<int>(3, value: 3);
 
-final node1=Node(value1);
-final node2=Node(value2);
-final node3=Node(value3);
+  node1.next = node2;
+  node2.next = node3;
 
-node1.next=node2;
-node2.next=node3;
-
-print(node1);
+  print(node1); // Output: 1 -> 2 -> 3
+}
