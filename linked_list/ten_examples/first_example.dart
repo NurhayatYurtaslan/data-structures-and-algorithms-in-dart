2fdaff3f977b5a69360f51ed52d298a405334016
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-08-09 08:50:44 +0300

    Create first_example.dart

---
+++ linked_list/ten_examples/first_example.dart
@@ -0,0 +1,49 @@
+void main() {
+  // Create a linked list
+  LinkedList list = LinkedList();
+
+  // Add elements to the linked list
+  list.add(10);
+  list.add(20);
+  list.add(30);
+
+  // Print the linked list
+  print('Linked List:');
+  list.printList();
+}
+
+// Node class to represent each element in the linked list
+class Node {
+  int value;
+  Node? next;
+
+  Node(this.value);
+}
+
+// LinkedList class to manage nodes
+class LinkedList {
+  Node? head;
+
+  // Method to add an element to the linked list
+  void add(int value) {
+    Node newNode = Node(value);
+    if (head == null) {
+      head = newNode;
+    } else {
+      Node current = head!;
+      while (current.next != null) {
+        current = current.next!;
+      }
+      current.next = newNode;
+    }
+  }
+
+  // Method to print the linked list
+  void printList() {
+    Node? current = head;
+    while (current != null) {
+      print(current.value);
+      current = current.next;
+    }
+  }
+}
