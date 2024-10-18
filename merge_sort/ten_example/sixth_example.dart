c66db259a5d14fdb29eeec7126147492788abf66
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-10-18 12:28:39 +0300

    Create sixth_example.dart

---
+++ merge_sort/ten_example/sixth_example.dart
@@ -0,0 +1,70 @@
+void merge(List<int> arr, int left, int mid, int right) {
+  int n1 = mid - left + 1;
+  int n2 = right - mid;
+
+  List<int> leftArray = List.filled(n1, 0);
+  List<int> rightArray = List.filled(n2, 0);
+
+  for (int i = 0; i < n1; i++) {
+    leftArray[i] = arr[left + i];
+  }
+  for (int j = 0; j < n2; j++) {
+    rightArray[j] = arr[mid + 1 + j];
+  }
+
+  int i = 0, j = 0, k = left;
+
+  while (i < n1 && j < n2) {
+    if (leftArray[i] <= rightArray[j]) {
+      arr[k] = leftArray[i];
+      i++;
+    } else {
+      arr[k] = rightArray[j];
+      j++;
+    }
+    k++;
+  }
+
+  while (i < n1) {
+    arr[k] = leftArray[i];
+    i++;
+    k++;
+  }
+
+  while (j < n2) {
+    arr[k] = rightArray[j];
+    j++;
+    k++;
+  }
+}
+
+void mergeSort(List<int> arr, int left, int right) {
+  if (left < right) {
+    int mid = left + (right - left) ~/ 2;
+
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+
+    merge(arr, left, mid, right);
+  }
+}
+
+void main() {
+  List<int> arr = [5, 8, 3, 12, 9, 6, 7, 4];
+  List<int> evens = [];
+  List<int> odds = [];
+
+  for (int num in arr) {
+    if (num % 2 == 0) {
+      evens.add(num);
+    } else {
+      odds.add(num);
+    }
+  }
+
+  mergeSort(evens, 0, evens.length - 1);
+  mergeSort(odds, 0, odds.length - 1);
+
+  print("Sorted evens: $evens");
+  print("Sorted odds: $odds");
+}
