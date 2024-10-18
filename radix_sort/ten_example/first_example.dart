a92cecc8e4291af1fba5506039dbacc0ed5359b3
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-10-18 12:33:54 +0300

    Create first_example.dart

---
+++ radix_sort/ten_example/first_example.dart
@@ -0,0 +1,44 @@
+void countingSort(List<int> arr, int exp) {
+  int n = arr.length;
+  List<int> output = List.filled(n, 0);
+  List<int> count = List.filled(10, 0);
+
+  for (int i = 0; i < n; i++) {
+    int index = (arr[i] ~/ exp) % 10;
+    count[index]++;
+  }
+
+  for (int i = 1; i < 10; i++) {
+    count[i] += count[i - 1];
+  }
+
+  for (int i = n - 1; i >= 0; i--) {
+    int index = (arr[i] ~/ exp) % 10;
+    output[count[index] - 1] = arr[i];
+    count[index]--;
+  }
+
+  for (int i = 0; i < n; i++) {
+    arr[i] = output[i];
+  }
+}
+
+void radixSort(List<int> arr) {
+  int max = arr.reduce((a, b) => a > b ? a : b);
+
+  for (int exp = 1; max ~/ exp > 0; exp *= 10) {
+    countingSort(arr, exp);
+  }
+}
+
+void main() {
+  List<int> numbers = [170, 45, 75, 90, 802, 24, 2, 66];
+
+  print("Before sorting:");
+  print(numbers);
+
+  radixSort(numbers);
+
+  print("After sorting:");
+  print(numbers);
+}
